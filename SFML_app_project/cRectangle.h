0cf64d067527eeefe7154c6634e2887446acb334
PajakKamil <58186963+PajakKamil@users.noreply.github.com>
2020-12-06 20:29:04 +0100

    A new class (Rectangle)

---
+++ SFML_app_project/cRectangle.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef _rectangle_h_
+
+#include <SFML/Graphics.hpp>
+
+#define _rectangle_h_
+class Rectangle : public sf::Drawable
+{
+public:
+	Rectangle();
+	virtual ~Rectangle();
+
+private:
+};
+
+#endif //_rectangle_h
